37d212968af42f41d5f6e7cf113fb0c176b76a7b
chiman <chiman@sci.fi>
2010-08-07 21:37:11 +0300

    Config parser added, but config values not yet used.

--- create_class.py
+++ create_class.py
@@ -11,9 +11,7 @@ class OverheadOptimizerException(Exception):
 
 
 class ClassTemplateProcessor(object):
-    def __init__(self, template_str):
-        self.template_str = template_str
-# example:
+    def __init__(self):
         self.namespaces = []
 
     def use_namespaces(self, namespaces):
@@ -29,9 +27,7 @@ class ClassTemplateProcessor(object):
             return '} // namespace %s' % ns_name
         return '\n'.join(ns_end(ns) for ns in reversed(self.namespaces))
 
-    def process_variable(self, variable_match):
-        variable_name = variable_match.group(1)
-
+    def process_variable(self, variable_name):
         if variable_name == 'NAMESPACES_BEG':
             return self.namespaces_begin_str()
         if variable_name == 'NAMESPACES_END':
@@ -49,28 +45,133 @@ class ClassTemplateProcessor(object):
     def set_class_name(self, class_name):
         self.class_name = class_name
 
+    def set_config(self, config):
+        self.config = config
+
+
+class OverheadOptimizerConfig(object):
+    def __init__(self):
+        self.init_values()
+
+    def set_md5len(self, value):
+        try:
+            value = int(value)
+            if 1 <= value <= 32:
+                self.values['md5len'] = value
+                return True
+        except:
+            pass
+        return False
+
+    def set_guardstyle(self, value):
+        if value in ('underscore', 'nounderscore'):
+            self.values['guardstyle'] = value
+            return True
+        return False
+
+    def set_guardformat(self, value):
+        self.values['guardformat'] = value
+        return True
+
+    def set_project(self, value):
+        self.values['project'] = value
+        return True
+
+    def set_projectroot(self, value):
+        self.values['projectroot'] = value
+        return True
+
+    def set_headerfolder(self, value):
+        self.values['headerfolder'] = value
+        return True
+
+    def set_sourcefolder(self, value):
+        self.values['sourcefolder'] = value
+        return True
+
+    def set_defaultnamespace(self, value):
+        self.values['defaultnamespace'] = value
+        return True
+
+    def set_indent(self, value):
+        self.values['indent'] = value.strip("'").strip('"').replace('\\t', '\t')
+        return True
+
+    def get(self, variable_name):
+        return self.values[variable_name]
+
+    def init_values(self):
+        self.values = {}
+        self.set_md5len(32)
+        self.set_guardstyle('underscore')
+        self.set_guardformat('_$PROJECT_$FILENAME_')
+        self.set_project('')
+        self.set_projectroot('.')
+        self.set_headerfolder('include')
+        self.set_sourcefolder('src')
+        self.set_defaultnamespace('')
+        self.set_indent('    ')
+
+    def parse_config_file(self, filename):
+        def process_row(row):
+            row = row.strip()
+            if row.startswith('#') or len(row) == 0:
+                return
+            try:
+                variable, value = row.split('=', 1)
+                getattr(self, 'set_' + variable)(value)
+                return
+            except:
+                print 'invalid config file row:', row
+                pass
+
+        try:
+            for row in open(filename):
+                process_row(row)
+        except:
+            return False
+        return True
+
 
 def create_class(name, namespace=None, baseclass=None, is_interface=False):
 
+    config = OverheadOptimizerConfig()
+    config.parse_config_file('config.sample')
+
     prog_path = os.path.dirname(__file__)
     try:
-        template_file = os.path.join(prog_path, 'templates', 'class.h.tpl')
-        template_str = open(template_file).read()
+        h_template_file = os.path.join(prog_path, 'templates', 'class.h.tpl')
+        h_template_str = open(h_template_file).read()
     except:
         raise OverheadOptimizerException('Could not open template file ' + \
-                                         '"%s"' % template_file)
+                                         '"%s"' % h_template_file)
+    try:
+        c_template_file = os.path.join(prog_path, 'templates', 'class.cpp.tpl')
+        c_template_str = open(c_template_file).read()
+    except:
+        raise OverheadOptimizerException('Could not open template file ' + \
+                                         '"%s"' % c_template_file)
 
-    ctp = ClassTemplateProcessor(template_str)
+    ctp = ClassTemplateProcessor()
+    ctp.set_config(config)
     ctp.set_class_name(name)
     ctp.set_baseclass(baseclass)
     if namespace:
         ctp.use_namespaces(namespace.split('::'))
     ctp.is_interface = is_interface
 
+    def process_variable(match):
+        return ctp.process_variable(match.group(1))
+
     class_h = open('%s.h' % name, 'w')
-    class_str = re.sub('##([^#]+)#', ctp.process_variable, template_str)
+    class_str = re.sub('##([^#]+)#', process_variable, h_template_str)
     class_h.write(class_str)
     class_h.close()
+
+    class_c = open('%s.cpp' % name, 'w')
+    class_str = re.sub('##([^#]+)#', process_variable, c_template_str)
+    class_c.write(class_str)
+    class_c.close()
 
 
 if __name__ == '__main__':
